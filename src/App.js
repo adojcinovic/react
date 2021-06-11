6a7f645879e7bf589f227dcc53e388d00d4df3d8
BIT Student <adojca@gmail.com>
2021-06-11 17:45:23 +0200

    react update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import { Header } from './header/Header'
+// import { Videos } from './videos/Videos'
+import React from 'react'
+import { useState } from 'react'
+
+
 
 function App() {
+
+  const [value, setValue] = useState(0)
+  const [nekitext, setInput] = useState('')
+
+  const getInput = () => {
+    setInput(nekitext)
+    console.log(nekitext);
+  }
+
+  const increase = () => {
+    setValue(value + 1)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header getText={getInput} input={nekitext} value={value} povecaj={increase} />
     </div>
   );
 }
---
+++ src/header/Header.js
@@ -0,0 +1,19 @@
+import './Header.css'
+
+
+function Header(props) {
+
+
+    return (
+        <div className='header'>
+            <input onChange={props.getText}></input>
+            <button onClick={props.povecaj}>increase</button>
+            <div>{props.value}</div>
+            <div>text:{props.input}</div>
+        </div>
+    )
+
+}
+
+
+export { Header }
